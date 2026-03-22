e0977a76756573bc932fdac820679a1e7927dc35
hernanmix <hernantv94@gmail.com>
2026-03-21 22:23:49 -0500

    Update segment1.ts

--- segment1.ts
+++ segment1.ts
@@ -2,10 +2,11 @@
 #EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-TARGETDURATION:6
-#EXT-X-MEDIA-SEQUENCE:100
+#EXT-X-MEDIA-SEQUENCE:0
 #EXTINF:6.000,
-segment100.ts
+https://hernanmix.github.io/reproductor/segment0.ts
 #EXTINF:6.000,
-segment101.ts
+https://hernanmix.github.io/reproductor/segment1.ts
 #EXTINF:6.000,
-segment102.ts
+https://hernanmix.github.io/reproductor/segment2.ts
+#EXT-X-ENDLIST
