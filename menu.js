4adb835519d8bda21eb692f193cc32770048d144
hernanmix <hernantv94@gmail.com>
2026-04-20 23:58:42 -0500

    Fix URL format for DONAR link in menu.js

--- menu.js
+++ menu.js
@@ -57,7 +57,7 @@ document.write(`
   </a>
 
   <!-- DONAR -->
-  <a href="URL_DONARhttps://hsports4hd.blogspot.com/2026/02/donar.html" class="nav-btn">
+  <a href="https://hsports4hd.blogspot.com/2026/02/donar.html" class="nav-btn">
     <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
       <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
     </svg>
